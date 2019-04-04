9694867cd03c2140eaafe503242476f40088cd5b
Brandon Sumner <sumnerd31@gmail.com>
2019-04-03 21:45:10 -0500

    Initial Commit

---
+++ Fitionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitionary
+//
+//  Created by Brandon Sumner on 4/3/19.
+//  Copyright © 2019 Brandon Sumner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
